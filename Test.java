fe4b478d76c82ef520e916cbde9232a7383ffc6d
darksonic2112 <83122333+darksonic2112@users.noreply.github.com>
2021-08-16 14:52:47 +0200

    Mini Taschenrechner implementiert

--- Test.java
+++ Test.java
@@ -4,6 +4,33 @@ public class Test {
 
     public static void main(String[] args){
 
-    System.out.println("Hi");
-}
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Gebe die erste Zahl ein: ");
+    int x = scanner.nextInt();
+    System.out.println("Wähle einen Operator aus: ");
+    String operator = scanner.next();
+    System.out.println("Gebe eine zweite Zahl ein: ");
+    int y = scanner.nextInt();
+
+    System.out.println("Dein Ergebnis ist: ");
+
+    switch(operator){
+        case "+":
+            System.out.println(x + y);
+            break;
+        case "-":
+            System.out.println(x - y);
+            break;
+        case "/":
+            System.out.println(x / y);
+            break;
+        case "*":
+            System.out.println(x * y);
+            break;
+        default:
+            System.out.println("Ungültige Angaben");
+        }
+    scanner.close();
+    }
 }
